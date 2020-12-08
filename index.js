f7c2f8ac19605bf028afb44d9b9c053dee728f35
xeusteerapat <xeus085@gmail.com>
2020-12-08 21:48:10 +0700

    [edit] setup socket both client and server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(9000, () => {
+  console.log('Server running on port 9000 🔥');
+});
+
+const io = socketio(server);
+
+io.on('connection', socket => {
+  socket.emit('messageFromServer', { data: 'Welcome to socket server' });
+  socket.on('messageToServer', dataFromClient => {
+    console.log(dataFromClient);
+  });
+});
