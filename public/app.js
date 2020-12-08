c1ded5ba60c84d11b9f5ba89c3f0b2f61dbc5f1c
xeusteerapat <xeus085@gmail.com>
2020-12-08 22:51:13 +0700

    [edit] submit message from client

--- public/app.js
+++ public/app.js
@@ -1,5 +1,26 @@
 const socket = io('http://localhost:9000');
+
+socket.on('connect', () => {
+  console.log(socket.id);
+});
+
 socket.on('messageFromServer', dataFromServer => {
   console.log(dataFromServer);
   socket.emit('messageToServer', { data: 'This is from the client' });
 });
+
+document.getElementById('message-form').addEventListener('submit', e => {
+  e.preventDefault();
+  const newMsg = document.querySelector('#user-message').value;
+
+  socket.emit('newMessageToServer', { text: newMsg });
+});
+
+// socket.on('ping', () => {
+//   console.log('ping was recieved from server');
+// });
+
+// socket.on('pong', latency => {
+//   console.log(latency);
+//   console.log('pong was sent to the server');
+// });
